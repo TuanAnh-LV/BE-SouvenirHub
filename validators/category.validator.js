4d805918bc54ad573c26533530d949ed4bc54bca
TuanAnh-LV <anhlvtse172914@fpt.edu.vn>
2025-05-27 15:18:50 +0700

    validate product, category

--- validators/category.validator.js
+++ validators/category.validator.js
@@ -6,9 +6,6 @@ exports.createCategoryValidator = [
     .isString().withMessage('Name must be a string')
     .isLength({ min: 2 }).withMessage('Name must be at least 2 characters'),
   
-  body('parent_id')
-    .optional({ nullable: true })
-    .isMongoId().withMessage('Invalid parent_id')
 ];
 
 // Middleware cho việc cập nhật category
@@ -18,7 +15,4 @@ exports.updateCategoryValidator = [
     .isString().withMessage('Name must be a string')
     .isLength({ min: 2 }).withMessage('Name must be at least 2 characters'),
 
-  body('parent_id')
-    .optional({ nullable: true })
-    .isMongoId().withMessage('Invalid parent_id')
 ];
--- validators/product.validator.js
+++ validators/product.validator.js
@@ -1,9 +1,7 @@
 const { body } = require('express-validator');
 
 exports.createProductValidator = [
-  body('name').isString().isLength({ min: 3 }),
   body('description').isString(),
   body('price').isFloat({ gt: 0 }),
   body('stock').isInt({ min: 0 }),
-  body('category_id').notEmpty().isMongoId()
 ];
